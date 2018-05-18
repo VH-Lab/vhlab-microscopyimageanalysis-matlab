8295127cd31c759c21f7c7861d700604e4450e74
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2018-05-18 16:30:51 -0400

    at_roi_sizes

--- at_exporters/at_roi_sizes.m
+++ at_exporters/at_roi_sizes.m
@@ -5,6 +5,8 @@ function ROIsize = at_roi_sizes(filename)
 %
 % Returns an array of the ROI sizes
 %
+% In addition, writes the data to a file called FILENAME_roisize.xls.
+%
 
 ROIsize = [];
 
@@ -13,5 +15,11 @@ a = load(filename,'-mat');
 for i=1:a.CC.NumObjects,
     ROIsize(i) = numel(a.CC.PixelIdxList{i});
 end;
+
+[filepath,fname,ext] = fileparts(filename);
+
+excelfilename = fullfile(filepath,[fname '_roisize.xls']);
+
+xlswrite(excelfilename, ROIsize);
 
 
