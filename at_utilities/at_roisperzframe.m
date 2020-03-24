6073deec17880fb5f214f7aa5438a449f28115b1
Steve Van Hooser <vanhoosr@brandeis.edu>
2020-03-24 09:58:16 -0400

    added at_roisperzframe, closes #35

---
+++ at_utilities/at_roisperzframe.m
@@ -0,0 +1,28 @@
+function [rois_per_zframe] = at_roisperzframe(the_atdir, roiname)
+% AT_ROISPERZFRAME - calculate the number of ROIs that intersect each Z frame of the stack
+%
+% ROIS_PER_ZFRAME = AT_ROISPERZFRAME(THE_ATDIR, ROINAME)
+%
+% Given an array tomography directory object (see "help atdir") and an ROI dataset name in
+% ROINAME, this function calculates the number of 3D ROIs that intersect each Z plane in 2D.
+%
+% Example:
+%     the_atdir = atdir('/Users/vanhoosr/Downloads/2015-06-03');
+%     roiname = 'PSD_newscale_th_roi_vf_res'; % example roi name
+%     rois_per_zframe = at_roisperzframe(the_atdir, roiname);
+% 
+% See also: ATDIR 
+
+
+roifilename = getroifilename(the_atdir, roiname);
+roilabeledfilename = getlabeledroifilename(the_atdir, roiname);
+
+L = load(roilabeledfilename,'-mat');
+L = L.L;
+
+rois_per_zframe = [];
+
+for z=1:size(L,3), % over each z frame
+	unique_rois_here = setdiff(unique(L(:,:,z)),0);
+	rois_per_zframe(z) = numel(unique_rois_here);
+end;
