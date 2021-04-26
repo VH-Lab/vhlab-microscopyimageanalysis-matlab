51478fc75809020c371f5f6194c5f73519283973
Steve Van Hooser <vanhoosr@brandeis.edu>
2021-04-25 20:05:17 -0400

    filter2tbrightness

--- at_pipelines/specific_studies/wiseetal_validation.m
+++ at_pipelines/specific_studies/wiseetal_validation.m
@@ -18,3 +18,6 @@ at_foreachdirdo(d_here,'vh_groundtruthcompare(atd,''VG_DECsv1_roiresvf'',''spine
  
 % image2roi - 1:6, 13:23,  errors in 7:12
 % PSD_DECsv1 - 1:6, 13:23 in progress 
+
+at_foreachdirdo(d_here,'vh_filter2tbrightness(atd,''PSD_DECsv1_roires'',''PSD_DECsv1_roiresbf'');');
+at_foreachdirdo(d_here,'vh_filter2tbrightness(atd,''VG_DECsv1_roires'',''VG_DECsv1_roiresbf'');');
