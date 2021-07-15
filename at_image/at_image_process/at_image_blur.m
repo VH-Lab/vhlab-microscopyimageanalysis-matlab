227bc9fa4f7665eccca7af40e27bab4a4a32416f
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2021-07-15 08:11:54 -0400

    at_image_blur finalized

--- at_image/at_image_process/at_image_blur.m
+++ at_image/at_image_process/at_image_blur.m
@@ -9,13 +9,11 @@ function out = at_image_blur(atd, input_itemname, output_itemname, parameters)
 %  OUT{3} is a list of methods for user-guided selection of these parameters.
 %
  
-error('not debugged yet.');
-
 image_viewer_name = 'IM';
 
-if nargin==0,
+if nargin==0,                  % If function is called with 'zero' inputs, displays what parmaeters it takes. 
 	out{1} = {'useGaussian','radius','filtersize'};
-	out{2} = {'Filter type; 1=gaussian, 0=pure circle', 'Human 2','Human 3'};
+	out{2} = {'Filter type: 1=gaussian, 0=pure circle', 'Radius in pixels','Convolution filter in pixels'};
 	out{3} = {'choose_inputdlg'};
 	return;
 end;
@@ -23,34 +21,42 @@ end;
 if ischar(parameters),
 	switch lower(parameters),
 		case 'choose',
-			out_choice = at_image_threshold;
+			out_choice = at_image_blur();
 			choices = cat(2,out_choice{3},'Cancel');
 			buttonname = questdlg('By which method should we choose parameters?',...
 				'Which method?', choices{:},'Cancel');
 			if ~strcmp(buttonname,'Cancel'),
-				out = at_image_threshold(atd,input_itemname,output_itemname,buttonname);
+				out = at_image_blur(atd,input_itemname,output_itemname,buttonname);
 			else,
 				out = [];
 			end;
 
 		case {'choose_inputdlg'},
-			out_p = at_image_threshold;
-			default_parameters.threshold = 100;
+			out_p = at_image_blur();
+			default_parameters.useGaussian = 1;
+			default_parameters.radius = 20;
+			default_parameters.filtersize = 100;
 			parameters = dlg2struct('Choose parameters',out_p{1},out_p{2},default_parameters);
 			if isempty(parameters),
 				out = [];
 			else,
-				out = at_image_threshold(atd,input_itemname,output_itemname,parameters);
+				out = at_image_blur(atd,input_itemname,output_itemname,parameters);
 			end;
 	end;
 	return;
 end;
 
- % perform the thresholding
+ % perform the filtering
+
+if parameters.useGaussian,
+	filter_type_str = 'Gaussian';
+else,
+	filter_type_str = 'circular';
+end;
 
 h = gethistory(atd,'images',input_itemname);
 h(end+1) = struct('parent',input_itemname,'operation','at_image_blur','parameters',parameters,...
-	'description',['Applied blur with radius ' num2str(parameters.threshold) ' to image ' input_itemname '.']);
+	'description',['Applied ' filter_type_str ' blur with radius ' num2str(parameters.radius) ' and filtersize ' num2str(parameters.filtersize) ' to image ' input_itemname '.']);
 
 im_in_file = getimagefilename(atd,input_itemname);
 
@@ -64,12 +70,10 @@ extra_args{1} = {'WriteMode','overwrite'};
 extra_args{2} = {'WriteMode','append'};
 
 for i=1:length(input_finfo),
-	im = imread(im_in_file,'index',i,'info',input_finfo);
-	if i==1, try, mkdir([getpathname(atd) filesep 'images' filesep output_itemname]); end; end;
-    % Sam's code goes here:
-    % This is the thresholding thing
-    im = logical(im > parameters.threshold);
-	imwrite(im,im_out_file,extra_args{1+double(i>1)}{:});
+	im = imread(im_in_file,'index',i,'info',input_finfo);         % Reads the image
+	if i==1, try, mkdir([getpathname(atd) filesep 'images' filesep output_itemname]); end; end; % Makes new folder if needs too
+	im_out = vlt.image.circular_filter(im,parameters.useGaussian, parameters.radius, parameters.filtersize);     % Calls the circular_filter function and applies threshold to the image.
+	imwrite(im_out,im_out_file,extra_args{1+double(i>1)}{:});
 	str2text([getpathname(atd) filesep 'images' filesep output_itemname filesep 'parent.txt'], input_itemname);
 end;
 
