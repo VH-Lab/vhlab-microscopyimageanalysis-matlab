4ba12cf0b0774a027efd99cbad023d6127915610
Katherine Cheng <67941984+Xinyu-Cheng@users.noreply.github.com>
2021-09-29 12:26:05 -0400

    add colonization in mia

---
+++ +mia/colocalization/at_colocalization_makers_list.m
@@ -0,0 +1,22 @@
+function s = at_colocalization_makers_list
+% AT_COLOCALIZATION_MAKERS_LIST - List of available AT_COLOCALIZATION_MAKERS functions
+%
+%  S = AT_COLOCALIZATION_MAKERS_LIST
+%
+%  Returns in S a cell list of strings with the names of all available functions
+%  for making COLOCALIZATION analyses in array tomography images
+%
+
+dirname = fileparts(which('at_colocalization_shift')); % grab an example from the directory
+
+d = dir([dirname filesep '*.m']);
+
+s = {};
+
+for i=1:length(d),
+	if ~strcmp(lower(d(i).name),lower('Contents.m')),
+		[dummy,s{end+1,1},ext] = fileparts(d(i).name);
+	end;
+end;
+
+
