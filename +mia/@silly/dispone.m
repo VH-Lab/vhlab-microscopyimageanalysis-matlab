a8aeb793da651df304866c3bb3ae77282d968191
Katherine Cheng <67941984+Xinyu-Cheng@users.noreply.github.com>
2022-03-08 14:24:01 -0500

    delete the old at_ folders

--- +mia/@silly/dispone.m
+++
@@ -1,3 +0,0 @@
-function dispone(obj)
-
-disp('one');
--- at_utilities/automate/rundeconv.m
+++
@@ -1,7 +0,0 @@
-
-
-deconvolver4('C:\Users\Derek\Desktop\Analysis 5, spine 2\2 24 VV\2 24 VV TTX 1\analysis\images\PSD\PSD_frames.tif')
-deconvolver4('C:\Users\Derek\Desktop\Analysis 5, spine 2\2 24 VV\2 24 VV TTX 1\analysis\images\VG\VG_frames.tif')
-
-deconvolver4('C:\Users\Derek\Desktop\Analysis 5, spine 2\2 24 VV\2 24 VV TTX 2\analysis\images\PSD\PSD_scaled_30.tif')
-deconvolver4('C:\Users\Derek\Desktop\Analysis 5, spine 2\2 24 VV\2 24 VV TTX 2\analysis\images\VG\VG_scaled_30.tif')
